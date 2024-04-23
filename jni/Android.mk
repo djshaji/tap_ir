e1fc0ca1e8b0174520be52775f6362d80114f8bc
Shaji Khan <quotablecode@gmail.com>
2024-04-23 10:19:21 +0530

    first commit

---
+++ jni/Android.mk
@@ -0,0 +1,6 @@
+LOCAL_PATH := $(call my-dir) 
+include $(CLEAR_VARS) 
+LOCAL_MODULE := ir.lv2 
+LOCAL_SRC_FILES := ir.cc 
+include $(BUILD_SHARED_LIBRARY) 
+LOCAL_C_INCLUDES := dsp/ plugin/
---
+++ obj/local/arm64-v8a/objs/ir.lv2/ir.o.d
@@ -0,0 +1,5 @@
+./obj/local/arm64-v8a/objs/ir.lv2/ir.o: jni/ir.cc jni/ir.h jni/ir_utils.h
+
+jni/ir.h:
+
+jni/ir_utils.h:
